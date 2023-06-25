4c78a0bb0c257b3b464ef7550b404a5428dff182
MatusSandor <92015782+SandorMatus@users.noreply.github.com>
2023-06-25 22:07:07 +0200

    Add files via upload

---
+++ join.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+def combine_json_files(folder_path, output_file):
+    combined_data = []
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.json'):
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                combined_data.extend(data)
+
+    with open(output_file, 'w') as output:
+        json.dump(combined_data, output, indent=4)
+
+# Usage example:
+folder_path = './'  # Specify the folder containing the JSON files
+output_file = './output.json'  # Specify the output file path
+
+combine_json_files(folder_path, output_file)
